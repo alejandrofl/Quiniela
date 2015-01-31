0aa13931eca258586d1570d9d0c8ad6d00ea1cdd
alejandrofl <alejandro.fajardo.landeros@gmail.com>
2015-01-31 13:29:11 -0600

    view match forecast
    add view for match forecast

--- Scripts/quiniela-module.js
+++ Scripts/quiniela-module.js
@@ -1,2 +1,6 @@
 
-var quinielaModule=angular.module('quinielaModule',['ngRoute']);
+var quinielaModule=angular.module('quinielaModule',['ngRoute'])
+	.config(function($routeProvider,$locationProvider){
+		$routeProvider.when('/MatchForecast',{templateUrl:'/templates/Match_Forecast.html',controller : 'MatchForecastController'});
+		$locationProvider.html5Mode(true);
+	});
